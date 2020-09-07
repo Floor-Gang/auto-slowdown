1ca7647b9efc85d75b094fe4c61f67206dbd912d
Dovydas Zilvys <zilvysdovydas@yahoo.co.jp>
2020-09-07 07:16:17 +0100

    exclude + rmexclude command implementation

--- src/bot/commands.rs
+++ src/bot/commands.rs
@@ -1,18 +1,14 @@
-use crate::config::Config;
-use serenity::{
-    prelude::*,
-    framework::standard::{
-        CommandResult,
-        macros::{
-            command, group
-        }
-    },
-    model::{
-        channel::Message
-    }
-};
 use crate::bot::utils::*;
 use crate::bot::DataBase;
+// use crate::config::Config;
+use serenity::{
+    framework::standard::{
+        macros::{command, group},
+        Args, CommandResult, Delimiter,
+    },
+    model::channel::Message,
+    prelude::*,
+};
 
 #[group()]
 // #[commands(ping, db_test, prefix)]
@@ -21,14 +17,106 @@ pub struct Commands;
 
 #[command]
 async fn exclude(ctx: &Context, msg: &Message) -> CommandResult {
+    let mut args = Args::new(&msg.content, &[Delimiter::Single(' ')]);
+    match args.advance().single::<u64>() {
+        Ok(channel_id) => {
+            let channel_res = ctx.http.get_channel(channel_id).await;
+            match channel_res {
+                Ok(_) => {
+                    let data = ctx.data.read().await;
+                    let db = data.get::<DataBase>().unwrap();
 
+                    let res = db
+                        .execute(
+                            "INSERT INTO slow_mode.excluded_channels (channel_id) VALUES ($1)",
+                            &[&(channel_id as i64)],
+                        )
+                        .await;
+
+                    match res {
+                        Ok(_) => {
+                            reply(
+                                ctx,
+                                msg,
+                                &String::from("Channel has been successfully excluded"),
+                            )
+                            .await;
+                            return Ok(());
+                        }
+                        Err(_) => {
+                            reply(
+                                ctx,
+                                msg,
+                                &String::from("Channel already exists in the table"),
+                            )
+                            .await;
+                        }
+                    }
+                }
+                Err(_why) => {
+                    reply(ctx, msg, &String::from("Please enter a valid channel id")).await;
+                }
+            }
+        }
+        Err(_) => {
+            reply(ctx, msg, &String::from("Please enter a valid channel id")).await;
+        }
+    }
 
     Ok(())
 }
 
 #[command]
 async fn rmexclude(ctx: &Context, msg: &Message) -> CommandResult {
+    let mut args = Args::new(&msg.content, &[Delimiter::Single(' ')]);
+    match args.advance().single::<u64>() {
+        Ok(channel_id) => {
+            let channel_res = ctx.http.get_channel(channel_id).await;
+            match channel_res {
+                Ok(_) => {
+                    let data = ctx.data.read().await;
+                    let db = data.get::<DataBase>().unwrap();
 
+                    let res = db
+                        .execute(
+                            "DELETE FROM slow_mode.excluded_channels WHERE channel_id = $1 ",
+                            &[&(channel_id as i64)],
+                        )
+                        .await;
+
+                    match res {
+                        Ok(outcome) => {
+                            if outcome == 1 {
+                                reply(
+                                    ctx,
+                                    msg,
+                                    &String::from(
+                                        "Channel has been successfully removed from being excluded",
+                                    ),
+                                )
+                                .await;
+                            } else {
+                                reply(
+                                    ctx,
+                                    msg,
+                                    &String::from("Channel is not in the excluded table"),
+                                )
+                                .await;
+                            }
+                            return Ok(());
+                        }
+                        Err(why) => panic!(why),
+                    }
+                }
+                Err(_why) => {
+                    reply(ctx, msg, &String::from("Please enter a valid channel id")).await;
+                }
+            }
+        }
+        Err(_) => {
+            reply(ctx, msg, &String::from("Please enter a valid channel id")).await;
+        }
+    }
 
     Ok(())
 }
@@ -49,7 +137,6 @@ async fn rmexclude(ctx: &Context, msg: &Message) -> CommandResult {
 
 //     let row = db.query_one("SELECT channel_id FROM slow_mode.channels", &[]).await.unwrap();
 //     let channel_id: i64 = row.get(0);
-
 
 //     if let Err(why) = msg.channel_id.send_message(&ctx.http,  |m| {
 //         m.embed(|embed| {
--- src/bot/events.rs
+++ src/bot/events.rs
@@ -1,8 +1,5 @@
-use serenity::{
-    async_trait,
-    model::prelude::*,
-    prelude::*
-};
+use serenity::{async_trait, model::prelude::*, prelude::*};
+use crate::bot::utils::*;
 
 use crate::database::DataBase;
 
@@ -17,11 +14,29 @@ impl EventHandler for Handler {
     }
 
     async fn message(&self, ctx: Context, msg: Message) {
-        if msg.is_own(&ctx.cache).await {
-            return; 
+        if msg.is_own(&ctx.cache).await || excluded(&ctx, &msg).await {
+            return;
         }
+
         increment_channel(&ctx, &msg).await;
     }
+}
+
+async fn excluded(ctx: &Context, msg: &Message) -> bool {
+    let data = ctx.data.read().await;
+    let db = data.get::<DataBase>().unwrap();
+
+    let rows = db
+            .query("SELECT * FROM slow_mode.excluded_channels WHERE channel_id = $1", &[&(msg.channel_id.0 as i64)])
+            .await
+            .unwrap();
+
+    if rows.len() == 1 {
+        return true;
+    }
+
+    return false;
+
 }
 
 async fn increment_channel(ctx: &Context, msg: &Message) {
@@ -39,11 +54,12 @@ async fn increment_channel(ctx: &Context, msg: &Message) {
         Ok(_) => return,
         Err(_) => {
             let query_res = db
-                .query("UPDATE slow_mode.channels 
+                .query(
+                    "UPDATE slow_mode.channels 
                     SET message_count = message_count+1 
                     WHERE channel_id = $1",
                     &[&(msg.channel_id.0 as i64)],
-                    )
+                )
                 .await;
             if let Err(why) = query_res {
                 println!("Error updating text channels data: {:?}", why);
@@ -57,23 +73,31 @@ async fn check_messages(ctx: &Context) {
     let db = data.get::<DataBase>().unwrap();
 
     loop {
-        let rows = db.query("SELECT * FROM slow_mode.channels", &[]).await.unwrap();
+        let rows = db
+            .query("SELECT * FROM slow_mode.channels", &[])
+            .await
+            .unwrap();
         for row in rows {
-            let x : i64 = row.get(0);
+            let x: i64 = row.get(0);
             let z: i64 = row.get(1);
             let channel_id = x as u64;
             let message_count = z as u64;
 
-            if between(&message_count, 51,100) {
+            if between(&message_count, 51, 100) {
                 update_slow_mode(&ctx, &channel_id, 30).await;
-            } else if between(&message_count, 11,50) {
+            } else if between(&message_count, 11, 50) {
                 update_slow_mode(&ctx, &channel_id, 3).await;
-            } else if between(&message_count, 0,10) {
+            } else if between(&message_count, 0, 10) {
                 update_slow_mode(&ctx, &channel_id, 0).await;
             }
 
-
-            if let Err(why) = db.query("UPDATE slow_mode.channels SET message_count = 0 WHERE channel_id = $1", &[&(channel_id as i64)]).await {
+            if let Err(why) = db
+                .query(
+                    "UPDATE slow_mode.channels SET message_count = 0 WHERE channel_id = $1",
+                    &[&(channel_id as i64)],
+                )
+                .await
+            {
                 println!("Error updating text channels data: {:?}", why);
             }
         }
@@ -82,16 +106,13 @@ async fn check_messages(ctx: &Context) {
     }
 }
 
-async fn update_slow_mode(ctx: &Context,channel: &u64, seconds:u64) {
+async fn update_slow_mode(ctx: &Context, channel: &u64, seconds: u64) {
     let channel = ctx.http.get_channel(*channel).await.unwrap();
-    if let Err(why) = channel.id().edit(&ctx.http, |c| c.slow_mode_rate(seconds)).await {
+    if let Err(why) = channel
+        .id()
+        .edit(&ctx.http, |c| c.slow_mode_rate(seconds))
+        .await
+    {
         println!("Error setting channel's slow mode rate: {:?}", why);
     }
-}
-
-fn between(number: &u64, min: u64, max: u64) -> bool {
-    if min <= *number && *number <= max {
-        return true
-    }
-    return false
 }
